c6b25f77c2ec4ade08e0351025a2aa9c7ecb395a
prasanth <prasanth.mohan@cognizant.com>
2018-11-09 10:22:19 +0530

    First Commit

---
+++ src/main/java/com/pras/rabbitmqretries/RabbitmqretriesApplication.java
@@ -0,0 +1,12 @@
+package com.pras.rabbitmqretries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqretriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqretriesApplication.class, args);
+	}
+}
